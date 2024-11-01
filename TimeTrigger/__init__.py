21eeca10106f116b67693e2db1d318cd90e116a6
René Bremer <32130834+rebremer@users.noreply.github.com>
2024-11-01 09:34:43 +0100

    Update __init__.py

--- TimeTrigger/__init__.py
+++ TimeTrigger/__init__.py
@@ -37,6 +37,6 @@ def main(mytimer: func.TimerRequest) -> None:
     credential = DefaultAzureCredential()
     storage_account_url = "https://" + os.environ["par_storage_account_name"] + ".blob.core.windows.net"
     client = BlobServiceClient(account_url=storage_account_url, credential=credential)
-    blob_name = "test" + str(datetime.datetime.now()) + ".txt"
+    blob_name = "test" + str(datetime.now()) + ".txt"
     blob_client = client.get_blob_client(container=os.environ["par_storage_container_name"], blob=blob_name)
     blob_client.upload_blob(link_list)
